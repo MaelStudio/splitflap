fdf8c8597ea3a4e13c764c6e6bb3e90aac1c0c17
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2024-11-16 17:19:15 -0500

    Keep track of displayed char

--- Splitflap.ino
+++ Splitflap.ino
@@ -9,18 +9,23 @@ public:
   Module() {
 
     // CONSTANTS
-    flaps_count = 40; // how many flaps in module
+    flapsCount = 40; // how many flaps in module
     stepsPerRev = 2048; // how many steps per motor full revolution
-    stepsPerFlap = stepsPerRev / flaps_count; // steps required to rotate one character forward
-    stepInterval = 0; // time interval between each motor step in microseconds
+    stepsPerFlap = stepsPerRev / flapsCount; // steps required to rotate one character forward
+    stepInterval = 1700; // time interval between each motor step in microseconds
 
     // VARS
-    stepIdx = 0;
-    toStep = 0;
+    stepSequenceIdx = 0;
+    flapStepIdx = 0;
     lastStepTime = 0;
+    displayedIdx = 0;
+    moving = false;
+    offsetSteps = 0;
   }
 
-  void setup(int hallPin, int in1, int in2, int in3, int in4) {
+  void setup(int hallPin, int in1, int in2, int in3, int in4, int off) {
+
+    offset = off;
 
     // rearrange stepper pins in order IN1-IN3-IN2-IN4
     motorPins[0] = in1;
@@ -37,6 +42,7 @@ public:
 
   void tick() {
 
+    // reset before homing
     if (resetting) {
       if (step() && !isOnMagnet()) {
         resetting = false;
@@ -47,51 +53,59 @@ public:
     if (homing) {
       // try to step and check if home position is reached
       if (step() && isOnMagnet()) {
+        displayedIdx = 0;
+        displayed = chars[0];
+        flapStepIdx = 0;
         homing = false;
       }
       return; // finish homing before rotating to target character
     }
 
     // step until target character has been reached
-    if (toStep > 0 && step()) toStep--;
-  }
+    if (moving && step() && displayedIdx == targetIdx) {
+      offsetSteps = offset;
+      moving = false;
+    }
 
-  void home() {
-    homing = true;
+    if (offsetSteps && step()) {
+      offsetSteps--;
+    }
   }
 
   void reset() {
     resetting = true;
     home();
-    displayed = chars[0];
   }
 
   void display(char c) {
 
     c = toupper(c);
-    int targetIdx = findCharIdx(c);
-    int displayedIdx = findCharIdx(displayed);
+    targetIdx = findCharIdx(c);
 
     // loop back to home if target character is behind currently displayed
     if (targetIdx < displayedIdx) {
       home();
-      toStep = stepsPerFlap * targetIdx; // rotate to target character
-    } else {
-      toStep = stepsPerFlap * (targetIdx - displayedIdx); // rotate to target character
     }
-
-    displayed = c;
+    if (targetIdx != displayedIdx) {
+      moving = true;
+    }
+    
   }
 
 private:
   int motorPins[4];
   int sensorPin;
-  int flaps_count;
+  int flapsCount;
   int stepsPerRev;
   int stepsPerFlap;
   int stepInterval;
-  int stepIdx;
-  int toStep;
+  int stepSequenceIdx;
+  int flapStepIdx;
+  int displayedIdx;
+  int targetIdx;
+  int moving;
+  int offset;
+  int offsetSteps;
   unsigned long lastStepTime;
   bool resetting;
   bool stepSequence[4][4] = {
@@ -104,19 +118,34 @@ private:
 
   bool step() {
     unsigned long now = micros();
-    if (now - lastStepTime < 2000) return false;
+    if (now - lastStepTime < stepInterval) return false;
   
     // write correct sequence to step the motor
-    for (int in = 0; in < 4; in++) digitalWrite(motorPins[in], stepSequence[stepIdx][in]);
+    for (int in = 0; in < 4; in++) digitalWrite(motorPins[in], stepSequence[stepSequenceIdx][in]);
 
     // next in sequence
-    stepIdx++;
-    if (stepIdx == 4) {
-      stepIdx = 0;
+    stepSequenceIdx++;
+    if (stepSequenceIdx == 4) {
+      stepSequenceIdx = 0;
+    }
+
+    // flap step
+    flapStepIdx++;
+    if (flapStepIdx == stepsPerFlap) {
+      flapStepIdx = 0;
+      displayedIdx++;
+      if (displayedIdx == flapsCount) {
+        displayedIdx = 0;
+      }
+      displayed = chars[displayedIdx];
     }
     
     lastStepTime = now;
     return true;
+  }
+
+  void home() {
+    homing = true;
   }
 
   bool isOnMagnet() {
@@ -124,7 +153,7 @@ private:
   }
 
   int findCharIdx(char c) {
-    for (int i = 0; i < flaps_count; i++) if (chars[i] == c) return i;
+    for (int i = 0; i < flapsCount; i++) if (chars[i] == c) return i;
     return -1;
   }
 
@@ -138,9 +167,9 @@ public:
     modules = new Module[size];
   }
 
-  void setup(int pins[][5]) {
+  void setup(int pins[][6]) {
     for (int i = 0; i < size; i++) {
-      modules[i].setup(pins[i][0], pins[i][1], pins[i][2], pins[i][3], pins[i][4]);
+      modules[i].setup(pins[i][0], pins[i][1], pins[i][2], pins[i][3], pins[i][4], pins[i][5]);
       modules[i].reset();
     }
 
@@ -171,7 +200,7 @@ private:
 
   bool allModulesAtHome() {
     for (int i = 0; i < size; i++) {
-      if (modules[i].homing or modules[i].displayed != ' ') {
+      if (modules[i].homing || modules[i].displayed != ' ') {
         return false;
       }
     }
@@ -185,10 +214,10 @@ Display display(3);
 void setup() {
   Serial.begin(9600);
 
-  int pins[display.size][5] = {
-    {3, 4, 5, 6, 7},
-    {8, 9, 10, 11, 12},
-    {22, 24, 26, 28, 30}
+  int pins[display.size][6] = {
+    {3, 4, 5, 6, 7, 0},
+    {8, 9, 10, 11, 12, 20},
+    {22, 24, 26, 28, 30, 0}
   };
 
   Serial.println("Setting up modules...");
